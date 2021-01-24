bcb1acd0f88fe05b86bf98606122fe1de78cc4de
Jasnoor12 <77701828+Jasnoor12@users.noreply.github.com>
2021-01-25 10:55:40 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function spanel() {
+    window.alert("This is a Span element");
+}
+
+function divel() {
+    window.alert("This is a Div element");
+    document.getElementById("audio").play();
+}
